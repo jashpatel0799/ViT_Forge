916124f79a6130dca0cd591a6db952a9a55d9f02
jashpatel0799 <jashpatel8561@gmail.com>
2025-08-23 16:51:53 +0530

    make training curriculam learning compatible

--- train.py
+++ train.py
@@ -1,45 +1,63 @@
+"""
+Training/evaluation loops that support:
+- Fixed classifier head (100 logits) with *per-stage* class slicing.
+- W&B logging per stage (e.g., "MNIST/Train Loss").
+- Checkpoint saving hook (handled in main).
+"""
+
 import torch
+from torch import nn
 from tqdm.auto import tqdm
 import wandb
+from typing import Dict, Tuple, Optional
 
-
-def train_loop(model: torch.nn.Module, dataloader: torch.utils.data.DataLoader,
-               optimizer: torch.optim.Optimizer, loss_fn: torch.nn.Module, accuracy_fn, 
-               device: torch.device):
+@torch.no_grad()
+def _compute_accuracy(logits: torch.Tensor, target: torch.Tensor) -> float:
     """
-    training loop for pytorch model
+    Simple top-1 accuracy (avoid extra deps)
+    """
+    preds =logits.argmax(dim=1)
+    correct = (preds == target).sum().item()
+    return correct / target.numel()
+
+def train_loop(model: nn.Module, 
+               dataloader: torch.utils.data.DataLoader,
+               optimizer: torch.optim.Optimizer, 
+               loss_fn: nn.Module, 
+               device: torch.device,
+               num_current_classes: int
+) -> Tuple[float, float]:
+    """
+    Train model for one epoch on the current dataset subset of classes.
+    Only the first `num_classes_current` logits are supervised.
 
     Args:
         model (torch.nn.Module): Pytorch model to train
         dataloader (torch.utils.data.DataLoader): train set dataloader
         optimizer (torch.optim.Optimizer): optimizer to optimize the model
-        loss_fn (torch.nn.Module): loss function to calcualte loss
-        accuracy_fn (_type_): accuracy function tomeasure the accuracy
+        loss_fn (nn.Module): loss function to calcualte loss
         device (torch.device): device on which weight are load
-        
-    Return: 
-        train_model: train model weights
-        train_loss: loss value
-        train_acc: accuract\y value
+        num_current_classes (int): number of classes in curent dataset
         
     """
     
-    train_loss, train_acc = 0, 0
+    train_loss, train_acc, n_batches = 0, 0, 0
     
     model.train()
     
     for batch, (x_train, y_train) in enumerate(dataloader):
         # print("\nBATCH:", batch)
-        x_train, y_train = x_train.to(device), y_train.to(device)
+        x_train, y_train = x_train.to(device, non_blocking = True), y_train.to(device, non_blocking = True)
         
         # 1. Forward step
         train_pred = model(x_train)
+        train_logits = train_pred[:, :num_current_classes]
         
         # 2. Loss 
-        loss = loss_fn(train_pred, y_train)
+        loss = loss_fn(train_logits, y_train)
         
         # 3. Grad stepzero
-        optimizer.zero_grad()
+        optimizer.zero_grad(set_to_none=True)
         
         # 4. Backward
         loss.backward()
@@ -47,70 +65,86 @@ def train_loop(model: torch.nn.Module, dataloader: torch.utils.data.DataLoader,
         # 5. Optimizer step
         optimizer.step()
         
-        acc = accuracy_fn(torch.argmax(train_pred,dim=1), y_train)
+        acc = _compute_accuracy(train_logits, y_train)
         
         train_acc += acc
-        train_loss += loss
+        train_loss += loss.item()
+        n_batches += 1
         # print("ACC:", acc)
         # print("Loss:", loss)
         
     train_loss /= len(dataloader)
     train_acc /= len(dataloader)
     
-    return train_loss, train_acc, model
+    return train_loss / max(1, n_batches), train_acc / max(1, n_batches)
 
-def test_loop(model: torch.nn.Module, dataloader: torch.utils.data.DataLoader,
-              loss_fn: torch.nn.Module, accuracy_fn, device: torch.device):
+@torch.no_grad()
+def eval_loop(model: nn.Module, 
+              dataloader: torch.utils.data.DataLoader,
+              loss_fn: torch.nn.Module, 
+              device: torch.device,
+              num_current_classes: int
+) -> Tuple[float, float]:
     
     """
-    testing loop for pytorch model
+    Evaluate model for one pass on the current dataset test split,
+    slicing logits to the active class count.
 
     Args:
         model (torch.nn.Module): Pytorch model to test
         dataloader (torch.utils.data.DataLoader): test set dataloader
         loss_fn (torch.nn.Module): loss function to calcualte loss
-        accuracy_fn (_type_): accuracy function tomeasure the accuracy
         device (torch.device): device on which weight are load
-        
-    Return: 
-        test_loss: loss value
-        test_acc: accuract\y value
+        num_current_classes (int): number of classes in curent dataset
         
     """
     
-    test_loss, test_acc = 0, 0
+    eval_loss, eval_acc, n_batches = 0, 0, 0
     
     model.eval()
     with torch.inference_mode(): #with torch.no_grad:
-        for x_test, y_test in dataloader:
+        for x_eval, y_eval in dataloader:
             
-            x_test, y_test = x_test.to(device), y_test.to(device)
+            x_eval, y_eval = x_eval.to(device, non_blocking=True), y_eval.to(device, non_blocking=True)
             
-            test_pred = model(x_test)
+            eval_pred = model(x_eval)
             
-            loss = loss_fn(test_pred, y_test)
+            eval_logits = eval_pred[:, :num_current_classes]
             
-            acc = accuracy_fn(torch.argmax(test_pred, dim=1), y_test)
+            loss = loss_fn(eval_logits, y_eval)
             
-            test_loss += loss
-            test_acc += acc
+            acc = _compute_accuracy(eval_logits, y_eval)
             
-        test_loss /= len(dataloader)
-        test_acc /= len(dataloader)
+            eval_loss += loss
+            eval_acc += acc
+            n_batches += 1
+            
+        eval_loss /= len(dataloader)
+        eval_acc /= len(dataloader)
         
-    return test_loss, test_acc
+    return eval_loss / max(1, n_batches), eval_acc / max(1, n_batches)
         
         
         
 
-def train(model: torch.nn.Module, train_dataloader: torch.utils.data.DataLoader, 
-          test_dataloader: torch.utils.data.DataLoader, optimizer: torch.optim.Optimizer,
-          loss_fn: torch.nn.Module, accuracy_fn, epoches: int, device: torch.device, args):
+def train(stage_name: str,
+          model: torch.nn.Module, 
+          train_dataloader: torch.utils.data.DataLoader, 
+          test_dataloader: torch.utils.data.DataLoader, 
+          optimizer: torch.optim.Optimizer,
+          loss_fn: torch.nn.Module, 
+          epoches: int, 
+          device: torch.device,
+          num_current_classes: int, 
+          wandb_run = None
+) -> Dict[str, float]:
     
     """
-    Train and test the model
+    Train for `epochs` on a single dataset (stage). Logs per-epoch metrics.
+    Returns final metrics for the stage.
     
     Args: 
+        stage_name: in curriculam learning to track the DB
         model: A pytorch model to be train and test
         train_dataloader: a dataloader instance to train a model
         test_dataloader: a dataloader instance to test a model
@@ -127,37 +161,40 @@ def train(model: torch.nn.Module, train_dataloader: torch.utils.data.DataLoader,
     
     """
     
-    wandb.init(project=args['wandb_project'], name=args['wandb_runname'], config=args)
+    best_test_acc = 0.0
+    last_metrics: Dict[str, float] = {}
     
-    train_losses, train_acces = [], []
-    test_losses, test_acces = [], []
-    
-    for epoch in tqdm(range(epoches)):
+    for epoch in tqdm(range(1, epoches+1)):
         
-        train_loss, train_acc, model = train_loop(model=model, dataloader=train_dataloader, 
-                                                   optimizer=optimizer, loss_fn=loss_fn, 
-                                                   accuracy_fn=accuracy_fn, device=device)
+        train_loss, train_acc = train_loop(model=model, dataloader=train_dataloader, 
+                                            optimizer=optimizer, loss_fn=loss_fn, device=device, 
+                                            num_current_classes=num_current_classes)
         
         
-        test_loss, test_acc = test_loop(model=model, dataloader=test_dataloader, loss_fn=loss_fn,
-                                   accuracy_fn=accuracy_fn, device=device)
+        eval_loss, eval_acc = eval_loop(model=model, dataloader=test_dataloader, loss_fn=loss_fn,
+                                        device=device, num_current_classes=num_current_classes)
+        
+        if wandb_run is not None:
+            wandb_run.log({
+                f"{stage_name}/Train Loss": train_loss,
+                f"{stage_name}/Test Loss": eval_loss,
+                f"{stage_name}/Train Accuracy": train_acc,
+                f"{stage_name}/Test Accuracy": eval_acc,
+                f"{stage_name}/Epoch": epoch
+            })
+            
+        if eval_acc > best_test_acc:
+            best_test_acc = eval_acc
+        
+        print(f"\n[{stage_name}] Epoch: {epoch}/[{epoches}] \tTrain Loss: {train_loss:.5f} Test Loss: {eval_loss:.5f}  ||  Train Accuracy:  {train_acc:.5f}  Test Accuracy: {eval_acc:.5f}")
+        
+        last_metrics = {
+            "train_acc": train_acc,
+            "train_loss": train_loss,
+            "test_acc": eval_acc,
+            "test_loss": eval_loss,
+            "best_test_acc": best_test_acc
+        }
         
         
-        wandb.log({
-            "Train Loss": train_loss,
-            "Test Loss": test_loss,
-            "Train Accuracy": train_acc,
-            "Test Accuracy": test_acc
-        })
-        
-        print(f"\nEpoch: {epoch+1}/[{epoches+1}] ")
-        print(f"Train Loss: {train_loss:.5f}  Test Loss: {test_loss:.5f}  ||  Train Accuracy:  {train_acc:.5f}  Test Accuracy: {test_acc:.5f}")
-        
-        train_losses.append(train_loss.item())
-        test_losses.append(test_loss.item())
-        train_acces.append(train_acc.item())
-        test_acces.append(test_acc.item())
-        
-    wandb.finish()
-        
-    return model, train_losses, test_losses, train_acces, test_acces
+    return last_metrics
